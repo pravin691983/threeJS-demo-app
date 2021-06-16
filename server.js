6d9962fe6985eff4ea697ec6eeb7834f654ff5ce
Pravin Tawade <pravin.tawade@tieto.com>
2021-06-16 12:46:40 +0530

    Update server file to fixed issue

--- server.js
+++ server.js
@@ -4,12 +4,35 @@ const path = require("path");
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static("./dist/angular-three-js-demo-app"));
+// // Serve only the static files form the dist directory
+// app.use(express.static("./dist/angular-three-js-demo-app"));
 
-app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/angular-three-js-demo-app/" })
-);
+// app.get("/*", (req, res) =>
+//   res.sendFile("index.html", { root: "dist/angular-three-js-demo-app/" })
+// );
+
+// This middleware informs the express application to serve our compiled React files
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.use(
+    express.static(path.join(__dirname, "./dist/angular-three-js-demo-app"))
+  );
+
+  app.get("*", function (req, res) {
+    // res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(
+      path.resolve(
+        __dirname,
+        ".",
+        "dist",
+        "angular-three-js-demo-app",
+        "index.html"
+      )
+    );
+  });
+}
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
