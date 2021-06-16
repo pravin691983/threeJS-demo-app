8977eaabf7eeb87eb64c04dbe2f8a923d059a3f3
Pravin Tawade <pravin.tawade@tieto.com>
2021-06-16 11:50:58 +0530

    Update package file to deploy code Heruko

---
+++ angular-threeJS-demo-app/server.js
@@ -0,0 +1,13 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/angular-three-js-demo-app'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/angular-three-js-demo-app/index.html'));
+});
