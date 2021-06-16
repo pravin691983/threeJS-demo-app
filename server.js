faeeff5fe76223adf79b05552af32162c6ec844e
Pravin Tawade <pravin.tawade@tieto.com>
2021-06-16 12:25:07 +0530

    Update package file to fixed deploy issue

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/angular-three-js-demo-app"));
+app.use(express.static("./dist/angular-three-js-demo-app"));
 
-app.get("/*", function (req, res) {
-  res.sendFile(
-    path.join(__dirname + "/dist/angular-three-js-demo-app/index.html")
-  );
-});
+app.get("/*", (req, res) =>
+  res.sendFile("index.html", { root: "dist/angular-three-js-demo-app/" })
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
